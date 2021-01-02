20c6b12c2c110983022ea5ab189a875227509fb1
msu <mksus@yahoo.com>
2021-01-02 21:02:31 +0300

    update app.js functional

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,8 @@
-import React, { useRef, useEffect } from 'react'
-var k =  document.getElementById('k').value;
-var m = document.getElementById('m').value;
+import React, { useRef, useEffect, useState } from 'react'
+// var k =  document.getElementById('k').value;
+// var m = document.getElementById('m').value;
+let k = 50;
+let m = 10;
 var y = 0;
 var t = 1;
 var g = 9.8;
@@ -9,46 +11,81 @@ var dt = 0.11;
 var F;
 var a = 1;
 
-const Canvas = props => {
-  
-  const canvasRef = useRef(null)
-  
-  const draw = ctx => {
+let prevTime = undefined;
+let dynamicData = {
+  k: k,
+};
+
+
+const App = props => {
+
+  const canvasRef = useRef(null);
+
+  // const [stateK, setStateK] = useState(k);
+
+  const draw = (ctx, dt=0.11) => {
     t = t + dt;
-		F = 5*m*g - (0.05 * k)*y;
+    let dynamicK = dynamicData.k;
+    console.log('dynamic K', dynamicData);
+		F = 5*m*g - (0.05 * dynamicK)*y;
 		a = F/m;
 		v = v + a*dt;
     y = y + v*dt + a*(dt*dt)/2;
     ctx.clearRect(0, 0, 300, 800);
 		ctx.fillStyle = "grey";
-		ctx.fillRect(149.7 - (k / 30), 0, 1 + (k / 15), 75 + y);
+		ctx.fillRect(149.7 - (dynamicK / 30), 0, 1 + (dynamicK / 15), 75 + y);
 		ctx.fillStyle = "black";
     ctx.fillRect((300 - 10 * m) / 2, 75 + y, 10 * m, 20 * m);
-  }
+  };
 
-
-  useEffect(() => {
-    
-    const canvas = canvasRef.current
+  const render = (timestamp) => {
+    let deltaT = !!prevTime ? timestamp - prevTime : 0;
+    prevTime = timestamp;
+    const canvas = canvasRef.current;
     const context = canvas.getContext('2d')
     canvas.width = 300;
     canvas.height = 800;
-    let animationFrameId
-    
-    //Our draw come here
-    const render = () => {
-      draw(context)
-      animationFrameId = window.requestAnimationFrame(render)
-    }
-    render()
+    draw(context, deltaT/1000);
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId)
-    }
-  }, [draw])
-  return <canvas ref={canvasRef} {...props}/>
+    window.requestAnimationFrame(render);
+  };
+
+  const onInputChange = (e) => {
+    const inputValue = e?.target?.value;
+    console.log('debug on event', inputValue);
+    // setStateK(inputValue);
+    dynamicData.k = inputValue;
+
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    // console.log('call use effect');
+    // let firstInput = document.getElementById('k');
+    // console.log('first input', firstInput);
+    //
+    // firstInput.onchange = (e) => console.log('debug on event', e?.target?.value);
+
+    render()
+  }, []);
+
+  return (
+    <>
+      <div>
+        <h1 className ="top">Пружинный маятник</h1>
+        <p>Коэффициент упругости k = </p>
+
+        <input id="k" type="number" min="1" max="121" step="10" value={dynamicData.k} onChange={onInputChange}/>
+
+        <p>Масса груза m =   кг</p> <input id="m" type="number" value="10" min="0.1" max="10" step="1" />
+
+        <button id="mainButton">ЗАПУСК</button>
+      </div>
+      <canvas ref={canvasRef} {...props}/>
+    </>
+  )
 }
 
 
 
-export default Canvas
+export default App
